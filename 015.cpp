c9dc59e236b06dfd968cef3099cc37382facaf2a
younus-17 <younusshihab250@gmail,com>
2025-11-30 02:14:48 +0600

    Find the Size of int, float, double, and char

---
+++ 015.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    return 0;
+}
