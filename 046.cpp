2184b7f63f04d5f733c0cf7a29ef1fb60b71c738
younus-17 <younusshihab250@gmail,com>
2025-11-30 23:27:34 +0600

    Find the Largest Element in an Array

---
+++ 046.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i=0;i<n;i++) cin >> arr[i];
+
+    int largest = arr[0];
+    for(int i=1;i<n;i++)
+        if(arr[i] > largest) largest = arr[i];
+
+    cout << "Largest element: " << largest << endl;
+    return 0;
+}
