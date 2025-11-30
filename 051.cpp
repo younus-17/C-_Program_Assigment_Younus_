fb6f57c4eed6c6c54b003450903e9e3fff6e30ee
younus-17 <younusshihab250@gmail,com>
2025-11-30 23:52:13 +0600

    Sort an Array Using Insertion Sort

---
+++ 051.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i=0;i<n;i++) cin >> arr[i];
+
+    for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j = i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for(int i=0;i<n;i++) cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
