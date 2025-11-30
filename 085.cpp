ff3ff44443afac3741cb42387f36a0985c92c4fa
younus-17 <younusshihab250@gmail,com>
2025-12-01 03:18:32 +0600

    Swap Two Numbers Using Pointers

---
+++ 085.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void swapNumbers(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    cout << "Enter two numbers: ";
+    cin >> x >> y;
+
+    cout << "Before swapping: x = " << x << ", y = " << y << endl;
+    swapNumbers(&x, &y);
+    cout << "After swapping: x = " << x << ", y = " << y << endl;
+
+    return 0;
+}
