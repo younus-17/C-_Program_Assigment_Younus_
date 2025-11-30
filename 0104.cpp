380df080807aee61570486b54dc4104a2a45f9cd
younus-17 <younusshihab250@gmail,com>
2025-12-01 04:01:38 +0600

    Find Square and Cube of a Number

---
+++ 0104.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Square: " << num * num << endl;
+    cout << "Cube: " << num * num * num << endl;
+    return 0;
+}
