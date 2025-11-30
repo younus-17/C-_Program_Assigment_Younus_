ca274081f58d39dc8349948d94737a13dfbe2e54
younus-17 <younusshihab250@gmail,com>
2025-11-30 22:54:14 +0600

    Print Character Pattern

---
+++ 041.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    char ch = 'A';
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
