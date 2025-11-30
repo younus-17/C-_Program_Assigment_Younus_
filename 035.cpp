35df20a350f476d13ab255e7920938fb4efd94c0
younus-17 <younusshihab250@gmail,com>
2025-11-30 19:39:17 +0600

    Display Prime Numbers between Intervals

---
+++ 035.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
+}
+
+int main() {
+    int lower, upper;
+    cout << "Enter two numbers (lower and upper): ";
+    cin >> lower >> upper;
+
+    cout << "Prime numbers between " << lower << " and " << upper << " are: ";
+    for (int i = lower; i <= upper; i++) {
+        if (isPrime(i))
+            cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
